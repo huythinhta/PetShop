f728767aa848f9af73d1690a8daecf7c1f3cda11
Phong <phongdev0311@gmail.com>
2020-12-09 16:00:13 +0700

    Billdetail + status

--- src/main/java/pet/petshop/repository/BillInfoRepository.java
+++ src/main/java/pet/petshop/repository/BillInfoRepository.java
@@ -1,13 +1,18 @@
 package pet.petshop.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import pet.petshop.entity.Bill;
 import pet.petshop.entity.BillInfo;
 
 
 
 @Repository
 public interface BillInfoRepository extends JpaRepository<BillInfo, Integer>{
+	
+	List<BillInfo> findByBill(Bill bill);
 }
